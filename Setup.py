cbef9ae75f709f7057c405e858c15b8857ddbfef
EthanHarv <github@ethanharv.com>
2020-10-18 17:03:33 -0500

    Add lynis

--- Setup.py
+++ Setup.py
@@ -1,4 +1,6 @@
-#https://hastebin.com/raw/ezolofojev
+# Based on my notes https://hastebin.com/raw/ezolofojev
+# Recommended run command: sudo python3 Setup.py | tee SetupOutput.txt
+
 import configparser
 import subprocess
 
@@ -7,7 +9,7 @@ config.read('config.ini')
 
 if config['firefox'].getboolean('install-newest'):
     print("================================================")
-    print("Firefox: Installing")
+    print("Firefox: Installing Newest Version")
     print("================================================")
     subprocess.call(['sudo', 'add-apt-repository', 'ppa:ubuntu-mozilla-security/ppa'])
     subprocess.call(['sudo', 'apt', 'update',])
@@ -33,3 +35,18 @@ if config['ansible'].getboolean('run-roles'):
     print("Ansible: Running Roles")
     print("================================================")
     subprocess.call(['sudo', 'ansible', '/etc/ansible/roles/Harden.yml'])
+
+if config['lynis'].getboolean('install'):
+    print("================================================")
+    print("Lynis: Installing")
+    print("================================================")
+    subprocess.call(['sudo', 'apt-key', 'adv', '--keyserver', 'keyserver.ubuntu.com', '--recv-keys', 'C80E383C3DE9F082E01391A0366C67DE91CA5D5F'])
+    subprocess.call(['sudo', 'add-apt-repository', '"deb [arch=amd64] https://packages.cisofy.com/community/lynis/deb/ xenial main"'])
+    subprocess.call(['sudo', 'apt', 'update',])
+    subprocess.call(['sudo', 'apt', 'install', 'lynis'])
+
+if config['lynis'].getboolean('audit'):
+    print("================================================")
+    print("Lynis: Installing")
+    print("================================================")
+    subprocess.call(['sudo', 'lynis', 'audit', 'system', '|', 'tee', 'LynisAudit.txt'])

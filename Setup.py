f7f8bfdf8ed8d9474403ba24e8a1eb3884711127
EthanHarv <github@ethanharv.com>
2020-10-18 17:42:49 -0500

    Password check, update lynis

--- Setup.py
+++ Setup.py
@@ -7,6 +7,17 @@ import subprocess
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+print("================================================")
+print("ARE YOU SURE YOU HAVE A PASSWORD WITH 1 CREDIT IN ALL CATEGORIES? (At least 1 uppercase, 1 lowercase, 1 number, 1 special, 8+ characters total.)")
+print("================================================")
+
+confirm = input('yes/no: ')
+confirm = confirm.lower()
+
+if (confirm != "yes" or confirm != 'y'):
+    print("Quitting")
+    exit()
+
 if config['firefox'].getboolean('install-newest'):
     print("================================================")
     print("Firefox: Installing Newest Version")
@@ -41,7 +52,7 @@ if config['lynis'].getboolean('install'):
     print("Lynis: Installing")
     print("================================================")
     subprocess.call(['sudo', 'apt-key', 'adv', '--keyserver', 'keyserver.ubuntu.com', '--recv-keys', 'C80E383C3DE9F082E01391A0366C67DE91CA5D5F'])
-    subprocess.run('sudo apt-add-repository "deb [arch=amd64] https://packages.cisofy.com/community/lynis/deb/ xenial main"', shell=True)
+    subprocess.run('sudo apt-add-repository "deb [arch=amd64] https://packages.cisofy.com/community/lynis/deb/ xenial main"', shell=True) # Dunno *why* it has to be different here, the quotes just wouldn't play nicely with subprocess.call()
     subprocess.call(['sudo', 'apt', 'update',])
     subprocess.call(['sudo', 'apt', 'install', 'lynis'])
 
@@ -49,4 +60,4 @@ if config['lynis'].getboolean('audit'):
     print("================================================")
     print("Lynis: Installing")
     print("================================================")
-    subprocess.call(['sudo', 'lynis', 'audit', 'system', '|', 'tee', 'LynisAudit.txt'])
+    subprocess.call(['sudo', 'lynis', 'audit', 'system'])
